b7b5bc7da8e19bffbd5e78c3ee6612e096c032bd
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 18:01:11 +0900

    Table - パディング/コンテナ - パディング

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   const tablerow1 = TableRow(
         children: [ Text('アイテム1'), Text('A'), Text('300'), ],);
+
   const tablerow2 = TableRow(
         decoration: BoxDecoration(
           color: Colors.yellow,),
         children: [ Text('アイテム2'), Text('B'), Text('200'), ],);
+
   const tablerow3 = TableRow(
         children: [ Text('アイテム3'), Text('A'), Text('100'), ],);
+
 
   final table = Table(
     columnWidths: const {
@@ -23,8 +26,12 @@ void main() {
       tablerow3,
     ],
   );
-
-  final body = table;  
+ 
+  final body = Container( // ボディー
+    padding: EdgeInsets.all(30),
+    color: Colors.blue[50],
+    child: table
+  );  
 
   final sc = Scaffold(
     body: body, // ボディー        
