ec73e81a3574edfecdcc023784706302294af69c
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 18:02:26 +0900

    Table - スクロール - スクロールなし

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  const tablerow1 = TableRow(
-        children: [ Text('アイテム1'), Text('A'), Text('300'), ],);
-
-  const tablerow2 = TableRow(
-        decoration: BoxDecoration(
-          color: Colors.yellow,),
-        children: [ Text('アイテム2'), Text('B'), Text('200'), ],);
-
-  const tablerow3 = TableRow(
-        children: [ Text('アイテム3'), Text('A'), Text('100'), ],);
-
-  final tablerow4 = TableRow(
-        decoration: const BoxDecoration(
-          color: Colors.grey
-        ),
-        children: <Widget>[
-          Container(
-            height: 300,
-            width: 500,
-            color: Colors.red[50],
-          ),
-          Container(
-            height: 100,
-            width: 250,
-            color: Colors.purple[50],
-          ),
-          Container(
-            height: 200,
-            width: 250,
-            color: Colors.green[50],
-          ),
-        ],
-      );
+  
+  List<TableRow> tablerowList = []; // スクロール検証用
+  for(int i = 1; i <= 100; i++) {
+    tablerowList.add(
+     TableRow(
+      children: [ Text('アイテム$i'), const Text('A'), const Text('100'), ],)
+    );
+  }
 
   final table = Table(
-    columnWidths: const {
-      0: FlexColumnWidth(1),
-      1: FlexColumnWidth(3),
-      2: FlexColumnWidth(2),
-    },
-    border: TableBorder.all(width: 2,color: Colors.blue),
-    children: [
-      tablerow1,
-      tablerow2,
-      tablerow3,
-      tablerow4,
-    ],
+    border: TableBorder.all(),
+    children: tablerowList,
   );
- 
+
   final body = Container( // ボディー
-    padding: EdgeInsets.all(30),
-    color: Colors.blue[50],
+    padding: const EdgeInsets.all(30),
     child: table
   );  
 
